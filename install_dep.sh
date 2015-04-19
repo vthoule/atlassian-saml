120e3be44db64303bb1d87b8bc1320129ff010d9
Erik Gustavson <erik@bitium.com>
2015-04-19 15:26:29 -0700

    fix perms on install script; add packaging targetwq

--- install_dep.sh
+++ install_dep.sh
@@ -1,1 +1,2 @@
+mvn package
 mvn install:install-file -Dfile=target/SAML2Commons-1.0-SNAPSHOT.jar -DgroupId=com.bitium.saml -DartifactId=SAML2Commons -Dpackaging=jar -Dversion=1.0 -DgeneratePom=true
